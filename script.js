756ae486a7c5984c339713f4dc901dc8528914e1
sahityatiwari <sahityatiwari90035@gmail.com>
2024-05-09 09:25:04 +0530

    commit2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function print(){
     console,console.log(("hello"));
 }
+function print(){
+    console,console.log(("hesb gfbllo"));
+}
+function print(){
+    console,console.log(("hejsb bllo"));
+}
+function print(){
+    console,console.log(("helksnksklo"));
+}
