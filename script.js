0856a654a217cb0c63a7931f9a3eb1e1de46daff
sahityatiwari <sahityatiwari90035@gmail.com>
2024-05-09 14:52:01 +0530

    commit3

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 function print(){
     console,console.log(("hello"));
 }
-function print(){
-    console,console.log(("hesb gfbllo"));
-}
-function print(){
-    console,console.log(("hejsb bllo"));
-}
-function print(){
-    console,console.log(("helksnksklo"));
+
+
+function mul3(){
+    return 3;
 }
